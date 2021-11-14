4537371198a599fb91db7d6d4d5e06375ab540a9
Stephanie Li <listeph@gmail.com>
2021-11-13 22:00:38 -0600

    sidebar

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.write("TEst")
+st.write("Test")
+
+# sidebar 
+add_multiselect = st.sidebar.multiselect(
+    'School',
+    ['All', 'AH', 'ATEC', 'BBS' ,'ECS', 'EPPS', 'IS'
+     'NSM', 'SOM']
+)
+
+add_checkbox = st.sidebar.checkbox(
+    'Show classes'
+)
+
+add_checkbox = st.sidebar.checkbox(
+    'Show events'
+)
+
