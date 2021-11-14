77d2d0cb1caba28e939a1e59ddc3805db242446f
Tryston Minsquero <tam190005@utdallas.edu>
2021-11-14 10:48:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from DateSelector import event
 import data
 from Event import Event, getDayTimeConflicts
 import datetime as dt
+import plotly.graph_objects as go
+import numpy as np
 
 dat = data.Data()
 dat.assignMetaData()
@@ -50,9 +52,6 @@ filteredClasses = dat.filterEvents(dat.classes, termFilters, tagFilters)
 classesByDay = dat.getEventListByDay(filteredClasses)
 
 
-import plotly.graph_objects as go
-import datetime
-import numpy as np
 np.random.seed(1)
 
 week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
@@ -104,14 +103,4 @@ if show_classes:
     c1.plotly_chart(fig)
 if show_events:
     #c2.plotly_chart(fig)
-    
-
-# Plotly test
-import plotly.express as px
-data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20]]
-fig = px.imshow(data,
-                labels=dict(x="Day of Week", y="Time of Day", color="Productivity"),
-                x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-                y=['Morning', 'Afternoon', 'Evening']
-               )
-fig.update_xaxes(side="top")
+    "lol"
