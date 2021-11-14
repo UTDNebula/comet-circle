1d6b69922381b68d02415b79729f768135a97db4
Lucas Jaggernauth <lukejagg@gmail.com>
2021-11-13 23:55:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ data.assignTags()
 st.write("TEst")
 st.write(RenderTree(data.rootTag))
 
+st.write("Test")
+
+# sidebar 
+add_multiselect = st.sidebar.multiselect(
+    'School',
+    ['All', 'AH', 'ATEC', 'BBS' ,'ECS', 'EPPS', 'IS'
+     'NSM', 'SOM']
+)
+
+add_checkbox = st.sidebar.checkbox(
+    'Show classes'
+)
+
+add_checkbox = st.sidebar.checkbox(
+    'Show events'
+)
+
